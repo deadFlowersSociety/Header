ac93f6f51858c374beab408e1bd6ae90fb43ca5f
Chiijlaw <chiijlaw@berkeley.edu>
2018-10-01 11:25:55 -0700

    init postgresDB. Able to insert data from .json files into both cassandra and postgres.

--- database/insertData.js
+++ database/insertData.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
-const db = require("./cassandraDB.js");
+const db = require("./postgresqlDB.js");
 const JSONStream = require("JSONStream");
 const filepath =
-  "/Users/jones/Downloads/Hackreactor/hrsf101-system-design-capstone/Header/mockData3.json";
+  "/Users/jones/Downloads/Hackreactor/hrsf101-system-design-capstone/Header/mockData10.json";
 // const options = { flags: "r", encoding: "utf-8" };
 const pipeline = fs.createReadStream(filepath).pipe(JSONStream.parse());
 
